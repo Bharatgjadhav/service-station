ff11dda370424f8857856f001f93c18869208337
bharatjadhav <bharatgjadhav04@gmail.com>
2021-01-01 23:00:39 +0530

    modified files

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const bodyParser= require("body-parser")
-express = require("express")
+let express = require("express")
+let app =express()
 app.use(bodyParser.urlencoded({extended: false}))
 
 const sqlite3=require("sqlite3").verbose()
 const db= new sqlite3.Database("./service-station.db",(err)=>{
     if(err)
-    console.log("error in opening db    ")
+    console.log("error in opening db")
     else
     {
         console.log("db connected")
-        db.run(`CREATE TABLE OF NOT EXISTS USERS(USER_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                  NAME VARCHAR(30) NOTNULL,
+        db.run(`CREATE TABLE IF NOT EXISTS USERS(USER_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                  NAME VARCHAR(30) NOT NULL,
                                                   PHONE VARCHAR(10) UNIQUE NOT NULL,
-                                                  GENDER VARCHAR(2) NOTNULL,
-                                                  EMAIL_ID VARCHAR(30) NOTNULL,
-                                                  ADDRESS VARCHAR(50) NOTNULL,
-                                                  PASSWORD VARCHAR(10) NOTNULL)`,
+                                                  GENDER VARCHAR(2) NOT NULL,
+                                                  EMAIL_ID VARCHAR(30) NOT NULL,
+                                                  ADDRESS VARCHAR(50) NOT NULL,
+                                                  PASSWORD VARCHAR(10) NOT NULL)`,
                 (err)=> {
                                 if(err)
                                      console.log("could not create table",err)
@@ -42,17 +43,17 @@ app.get("/addusers",(req, res)=>{
 
 })
 
-app.post("/users",(req,res)=>{
+app.post("addusers", (req, res)=>{
     console.log("inside")
-    user_id = req.body.user_id
-    name  = req.body.name
-    phone = req.body.phone
-    gender = req.body.gender
-    email_id = req.body.email_id
-    address = req.body.address
-    password = req.body.password
-        db.run("INSERT INTO USERS(USER-ID,NAME,PHONE,GENDER,EMAIL-ID,ADDRESS,PASSWORD) VALUES(?,?,?,?,?,?,?)",[NULL, NAME,
-                    PHONE,GENDER,EMAIL-IDBCursor,ADDRESS,PASSWORD], (err)=>{
+    let user_id = req.body.user_id
+    let name  = req.body.name
+    let phone = req.body.phone
+    let gender = req.body.gender
+    let email_id = req.body.email_id
+    let address = req.body.address
+    let password = req.body.password
+        db.run("INSERT INTO USERS(USER-ID,NAME,PHONE,GENDER,EMAIL-ID,ADDRESS,PASSWORD) VALUES(?,?,?,?,?,?,?)",
+        [NULL, NAME, PHONE, GENDER, EMAIL_ID, ADDRESS, PASSWORD],(err)=>{
                         if(err){
                             console.log(err)
                             res.status(5000).render("addusers",{status : err})
@@ -63,7 +64,7 @@ app.post("/users",(req,res)=>{
                     })
 })
 
-app.get("/", (req,res)=>{      /* display users page */
+app.get("/service-station", (req, res)=>{      /* display users page */
         db.all("select *from users",(err, rows)=>{
             console.log(rows)
             if(err)
