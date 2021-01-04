449ac5c1e2c8c6d021a4570015216658ed34ee31
bharatjadhav <bharatgjadhav04@gmail.com>
2021-01-04 23:36:12 +0530

    added service database module and design

--- server.js
+++ server.js
@@ -21,9 +21,20 @@ const db= new sqlite3.Database("./service-station.db",(err)=>{
                                 if(err)
                                      console.log("could not create table",err)
                                 else{
-                                     console.log("table  created successfully ")
+                                     console.log(" USER table  created successfully ")
                                      }
                          })
+        db.run(`CREATE TABLE IF NOT EXISTS SERVICE (MECHANIC_ID INTEGER PRIMARYKEY AUTO INCREMENT,
+                                                   VEHICLE_NUMBER VARCHAR(25) UNIQUE NOT NULL, 
+                                                   SERVICE_DETAIL VARCHAR(100) NOT NULL,
+                                                   DATE VARCHAR(10) NOT NULL)`,
+                                            (err)=>{
+                                                if(err)
+                                                console.log("could not create table",err)
+                                                else{
+                                                    console.log(" service table created successfully")
+                                                }
+                                            })
     }
 })
 app.set("views","./views")
@@ -40,6 +51,9 @@ app.get("/home",(req, res)=>{
 
 app.get("/addusers",(req, res)=>{
     res.render("addusers",{status:0})
+})
+app.get("/service",(req,res)=>{
+    res.render("service",{status:1})
 })
 
 app.post("/addusers", (req, res)=>{
@@ -63,6 +77,24 @@ app.post("/addusers", (req, res)=>{
                             res.status(200).render("addusers",{status: "success"})
                         }
                     })
+})
+app.post("/service",(req,res)=>{
+    console.log("inside service")
+    let Mechanic_Id =req.body.Mechanic_Id
+    let Vehicle_Number=req.body.Vehicle_Number
+    let Service_Detail=req.body.Service_Detail
+    let Date=req.body.Date
+        db.run("INSERT INTO SERVICE(MECHANIC_ID,VEHICLE_NUMBER,SERVICE_DETAIL,DATE) VALUES(?,?,?,?)",
+        [Mechanic_Id,Vehicle_Number,Service_Detail,Date], (err)=>{
+            if(err){
+            console.log(err)
+            res.status(500).render("service",{status : err})
+            }
+        
+        else{
+            res.status(200).render("service",{status:"success"})
+        }
+    })
 })
 
 app.get("/users", (req, res)=>{      /* display users page */
